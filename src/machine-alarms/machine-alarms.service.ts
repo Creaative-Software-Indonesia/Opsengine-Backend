5c907548b95ec494489b8d9a1f92a6bee02f9bcc
Asep Trisna Setiawan <37206482+Aseptrisna@users.noreply.github.com>
2025-07-02 20:54:14 +0700

    Update machine-alarms.service.ts

--- src/machine-alarms/machine-alarms.service.ts
+++ src/machine-alarms/machine-alarms.service.ts
@@ -51,6 +51,7 @@ export class MachineAlarmService {
 
     async findAll(companyGuid: string, page: number = 1, limit: number = 10): Promise<any> {
         try {
+            if (page < 1) page = 1;
             const skip = (page - 1) * limit;
             const alarms = await this.machineAlarmModel.find({ companyGuid })
                 .skip(skip)
